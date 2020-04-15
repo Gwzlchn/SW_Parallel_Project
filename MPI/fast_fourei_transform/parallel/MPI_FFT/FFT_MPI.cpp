d72fab11925d91d14bee5149cf5a33056fa4b996
wangzl <wangzl1116@mails.jlu.edu.cn>
2020-04-15 23:04:51 +0800

    change array to ptr

--- MPI/fast_fourei_transform/parallel/MPI_FFT/FFT_MPI.cpp
+++ MPI/fast_fourei_transform/parallel/MPI_FFT/FFT_MPI.cpp
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<math.h>
 #include<stdio.h>
+#include<mpi.h>
 
 
 #ifdef _MSC_VER
@@ -12,14 +13,17 @@ typedef Complex cd;
 #endif // __MSVER_
 
 #ifdef __GNUC__
+
 #include<complex.h>
-typedef double complex  cd;
-typedef enum { false, true } bool;
+
+typedef double complex cd;
+
+typedef enum {
+    false, true
+} bool;
 #endif // __GNUC__
 
 const long double PI = M_PI;
-
-
 
 
 void Swap(cd* a, cd* b) {
@@ -29,18 +33,16 @@ void Swap(cd* a, cd* b) {
 }
 
 
-
-void Rader(cd y[], int len)
+void Rader(cd* y, int len) 
 {
+
     int i, j;
     int k = 0;
-    for (i = 1, j = len >> 1, k; i < len - 1; ++i)
-    {
+    for (i = 1, j = len >> 1, k; i < len - 1; ++i) {
         if (i < j)
             Swap(&y[i], &y[j]);
         k = len >> 1;
-        while (j >= k)
-        {
+        while (j >= k) {
             j -= k;
             k >>= 1;
         }
@@ -50,17 +52,13 @@ void Rader(cd y[], int len)
 }
 
 
-
-
-void fft(cd a[], int n, bool invert)
+void fft(cd* a, int n, bool invert) 
 {
-
-    Rader(a, n);
 
     int len;
     for (len = 2; len <= n; len <<= 1) {
         double ang = 2 * PI / len * (invert ? -1 : 1);
-        cd wlen  ( cos(ang) , sin(ang)) ;
+        cd wlen(cos(ang), sin(ang));
         int i = 0;
         for (i = 0; i < n; i += len) {
             cd w = 1;
@@ -83,15 +81,9 @@ void fft(cd a[], int n, bool invert)
     }
 }
 
-int* multiply_polys_seq_fft(int poly_a[], const int poly_b[], int a_len, int b_len, int* c_len)
+void init_poly_arr(const int* poly_a, const int* poly_b, int a_len, int b_len,
+                   cd* poly_a_cd,cd* poly_b_cd,int len)
 {
-
-    int len = 1;
-    while (len < a_len + b_len) len <<= 1;
-
-    cd* poly_a_cd = (cd*)malloc(sizeof(cd) * len);
-    cd* poly_b_cd = (cd*)malloc(sizeof(cd) * len);
-
     int i;
     for (i = 0; i < a_len; i++) {
         poly_a_cd[i] = poly_a[i];
@@ -106,19 +98,34 @@ int* multiply_polys_seq_fft(int poly_a[], const int poly_b[], int a_len, int b_l
         poly_b_cd[i] = 0;
     }
 
+   
+}
+
+int* multiply_polys_seq_fft(const int* poly_a, const int* poly_b, const int a_len, const int b_len, int* c_len) 
+{
+   
+    int len = 1;
+    while (len < a_len + b_len) len <<= 1;
+
+    cd* poly_a_cd =(cd*)malloc(sizeof(cd) * len);
+    cd* poly_b_cd = (cd*)malloc(sizeof(cd) * len);
+    init_poly_arr(poly_a, poly_b, a_len, b_len, poly_a_cd, poly_b_cd,len);
 
     //finding FFT of equations A and B separately
+    Rader(poly_a_cd, len);
+    Rader(poly_b_cd, len);
     fft(poly_a_cd, len, false);
     fft(poly_b_cd, len, false);
 
 
     //combing the FFT of A and B
     cd* F_C = (cd*)malloc(sizeof(cd) * len);
-
+    int i;
     for (i = 0; i < len; i++)
         F_C[i] = poly_a_cd[i] * poly_b_cd[i];
 
     //finding the inverse DFT and dividing it by 1/n
+    Rader(F_C, len);
     fft(F_C, len, true);
 
     int* res = (int*)malloc(sizeof(int) * len);
@@ -132,12 +139,26 @@ int* multiply_polys_seq_fft(int poly_a[], const int poly_b[], int a_len, int b_l
 }
 
 
-int main()
+int* multiply_polys_par_mpi_fft(int* poly_a, int* poly_b, int a_len, int b_len, int* c_len) 
 {
 
+    int len = 1;
+    while (len < a_len + b_len) len <<= 1;
 
-    int a[] = { 1,2,3,4,5,6,7,8 };
-    int b[] = { 1,2,3,4,5,6,7,8 };
+    cd* poly_a_cd = (cd*)malloc(sizeof(cd) * len);
+    cd* poly_b_cd = (cd*)malloc(sizeof(cd) * len);
+    init_poly_arr(poly_a, poly_b, a_len, b_len, poly_a_cd, poly_b_cd, len);
+    
+
+    return NULL;
+}
+
+
+int main() {
+
+
+    int a[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    int b[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
     int a_len = sizeof(a) / sizeof(a[0]);
     int b_len = sizeof(b) / sizeof(b[0]);
     int c_len = 0;
@@ -147,10 +168,6 @@ int main()
     for (i = 0; i < c_len; i++) {
         printf("%d  ", seq_fft_res[i]);
     }
-
-
-
-
 
 
     return 0;
